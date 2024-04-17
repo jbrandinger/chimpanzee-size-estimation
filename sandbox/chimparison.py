b6fc849e540aebaeb708e89fff7b03b08373c3a4
jbrandinger <98676769+jbrandinger@users.noreply.github.com>
2024-04-17 09:30:50 -0400

    some sandbox stuff

--- sandbox/chimparison.py
+++ sandbox/chimparison.py
@@ -2,40 +2,36 @@
 chimparison2.py
 '''
 
-# necessary imports
 import os
+import shutil
 import pandas as pd
 
-import shutil
+# Paths
+source_folder_path = '../../data/QC'
+spreadsheet_path = '../../data/measured.csv'
+destination_folder_path = '../../data/valid_QC'
 
-# helper function to remove '.jpg' labels
-def clean_image(image):
-    cleaned_image = image[:(len(image)-4)]
-    return cleaned_image
+# Read the spreadsheet to get image names
+spreadsheet_df = pd.read_csv(spreadsheet_path)
+names_column = list(spreadsheet_df['PhotoID'])
 
-folder_path = '../../data/'
+# Ensure the destination folder exists
+if not os.path.exists(destination_folder_path):
+    os.makedirs(destination_folder_path)
 
-images = []
-for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            images.append(clean_image(str(file).lower()))
-
-# get names of images in spreadsheet
-spreadsheet_df = pd.read_csv('../../measured.csv')
-names_column = spreadsheet_df['PhotoID']
-names_column_cleaned = []
-for image in names_column:
-    names_column_cleaned.append(str(image).lower())
-# print(names_column_cleaned)
-
+# Collect matched images and their paths
 collected_images = []
-for im in images:
-    if im in names_column_cleaned:
-        collected_images.append(im)
+for root, dirs, files in os.walk(source_folder_path):
+    for file in files:
+        name = str(file).split('.')[0]
+        if name in names_column:
+            # Append the file path to the list
+            collected_images.append(os.path.join(root, file))
+
+# Copy matched images to the new directory
+for file_path in collected_images:
+    shutil.copy2(file_path, destination_folder_path)
 
 print("Number of matched images: " + str(len(collected_images)))
-print("Ratio of matched images: " + str(len(collected_images)) + "/" + str(len(images)))
-# print(collected_images)
+print("Ratio of matched images: " + str(len(collected_images)) + "/" + str(len(names_column)))
 
-# for image in collected_images:
-#     shutil.copy(image, './destination2')
